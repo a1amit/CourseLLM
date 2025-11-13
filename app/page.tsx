a50f91ddad57bde81fd958bf445250fa6ac9a199
myMeaningfulName <104305953+myMeaningfulName@users.noreply.github.com>
2025-11-13 17:23:26 +0200

    ron's test

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,8 @@ export default function Home() {
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <div className="text-center text-2xl font-bold">
-            Hi Ron, Vladi and Gil
+            Hi Ron, Vladi and Gil,
+            Hi Amit!
           </div>
           <Hero />
           <main className="flex-1 flex flex-col gap-6 px-4">
