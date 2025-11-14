1f2d23c2bb7aad53232409b7610cdf8d5729cc2d
myMeaningfulName <104305953+myMeaningfulName@users.noreply.github.com>
2025-11-14 17:14:34 +0200

    ron second test

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <div className="text-center text-2xl font-bold">
             Hi Ron, Vladi and Gil,
-            Hi Amit!
+            Hi Amit! thank you for fixing git
           </div>
           <Hero />
           <main className="flex-1 flex flex-col gap-6 px-4">
